6946f49c41d329bcf34d487f3d75b0cf033cf1e6
Alexandre Gaïda <45820095+alxgda@users.noreply.github.com>
2023-06-26 14:15:04 +0200

    Update aliases.sh

--- shell config/bash/aliases.sh
+++ shell config/bash/aliases.sh
@@ -27,20 +27,25 @@ alias kcogc='kubectl config get-contexts' # display list of contexts
 alias kcocc='kubectl config current-context' # display the current-context
 alias kcouc='kubectl config use-context' # set the default context to the current cluster
 alias kcosc='kubectl config set-context --current' # Edit the current context properties
-alias kr='kubectl run' # help you quickly start a new pod
-alias kgd='kubectl get deployment' # list the deployments
-alias kcd='kubectl create deployment'
 alias kaf='kubectl apply -f' # apply the configuration from a YAML file
-alias kex='kubectl exec -i -t'
+alias kex='kubectl exec -it'
 alias kg='kubectl get' # list a ressource
-alias kgn2='alias kgn2='kubectl get nodes -o custom-columns=Hostname:".metadata.labels.kubernetes\.io/hostname",Name:".metadata.name",Role:".metadata.labels.node\.kubernetes\.io/role",Kubelet:.status.nodeInfo.kubeletVersion,Status:.status.conditions[-1].reason,Date:".metadata.creationTimestamp",InstType:".metadata.labels.beta\.kubernetes\.io/instance-type,Cpu Cores:.status.capacity.cpu,Ram:.status.capacity.memory,Taints:.spec.taints"''
-alias kgp='kubectl get pods' # list all pods in the current namespace
-alias kgs='kubectl get svc' # list all services in the current namespace
-alias kgsec='kubectl get secret'
 alias kd='kubectl describe' # get detailed information about a resource
-alias kdp='kubectl describe pod'
-alias kds='kubectl describe service'
-alias kdsec='kubectl describe secret'
 alias krm='kubectl delete' # delete a specified resource
 alias kga='kubectl get all' # list all resources in the current namespace
+alias kgn2='alias kgn2='kubectl get nodes -o custom-columns=Hostname:".metadata.labels.kubernetes\.io/hostname",Name:".metadata.name",Role:".metadata.labels.node\.kubernetes\.io/role",Kubelet:.status.nodeInfo.kubeletVersion,Status:.status.conditions[-1].reason,Date:".metadata.creationTimestamp",InstType:".metadata.labels.beta\.kubernetes\.io/instance-type,Cpu Cores:.status.capacity.cpu,Ram:.status.capacity.memory,Taints:.spec.taints"''
+alias kr='kubectl run' # help you quickly start a new pod
+alias kcd='kubectl create deployment'
+alias kgd='kubectl get deployments' 
+alias kdd='kubectl describe deployment'
+alias krmd='kubectl delete deployment'
+alias kcp='kubectl create pod'
+alias kgp='kubectl get pods' # list all pods in the current namespace
+alias kdp='kubectl describe pod'
+alias krmp='kubectl delete pod'
+alias kgs='kubectl get service' # list all services in the current namespace
+alias kds='kubectl describe service'
+alias krms='kubectl delete service'
+
+
 
