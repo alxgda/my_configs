375ae6c7c8ea64a3760c2e86a65a16c0ddead16b
Alexandre G <45820095+alxgda@users.noreply.github.com>
2023-08-20 11:05:58 +0200

    Update emacs_init.el

--- emacs_init.el
+++ emacs_init.el
@@ -2,51 +2,26 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (setq ns-use-native-fullscreen nil)
 
+;, Enable Native Smooth Scrolling (Emacs 29)
+(pixel-scroll-precision-mode 1)
+
 ;; Turn off some unneeded UI elements
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (tooltip-mode -1)
+(setq inhibit-startup-message t) ; Don't show the splash screen
 
-;; Don't show the splash screen
-(setq inhibit-startup-message t)
-
-;; Increase font size (too small for me by default)
-(set-face-attribute 'default nil
-                    :family "DejaVu Sans Mono"
-                    :height 155)
+;; start with an empty scratch buffer in Org mode.
+(setq initial-major-mode 'org-mode)
 
 ;; Wraps the lines visually for display purposes (do not split words...)
 (global-visual-line-mode t)
 
-;; Don't blink cursor
-(blink-cursor-mode 0)
-
-;; Set up the visible bell
-(setq visible-bell t)
+;; Blink cursor
+(blink-cursor-mode 1)
 
 ;; Add the colum position in the mod line
 (column-number-mode)
-
-;; Indent
-(setq-default tab-width 2
-      indent-tabs-mode  t
-      tab-always-indent t)
-
-;; mac switch meta key
-(defun mac-switch-meta nil
-  "switch meta between Option and Command"
-  (interactive)
-  (if (eq mac-option-modifier nil)
-      (progn
-	(setq mac-option-modifier 'meta)
-	(setq mac-command-modifier 'hyper)
-	)
-    (progn
-      (setq mac-option-modifier nil)
-      (setq mac-command-modifier 'meta)
-      )
-    )
-  )
 
 ;; Make sure it is all utf-8
 (prefer-coding-system 'utf-8)
@@ -59,9 +34,14 @@
 ;;(global-display-line-numbers-mode 1) to display lines everywhere
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 
-;; Set the  margins width (too small by default)
-(setq-default left-margin-width 1 right-margin-width 3)
-(set-window-buffer nil (current-buffer))
+;; Set the  margins width (too small by default) in org and markdown modes
+(defun my/set-margins-for-org-and-markdown ()
+  (when (or (derived-mode-p 'org-mode)
+            (derived-mode-p 'markdown-mode))
+     (setq-local left-margin-width 3)
+    (setq-local right-margin-width 3)
+    (set-window-buffer nil (current-buffer))))
+(add-hook 'find-file-hook 'my/set-margins-for-org-and-markdown)
 
 ;; Remember recently edited files
 (recentf-mode 1)
@@ -103,16 +83,30 @@
 ;; Set TRAMP default connection mode to SSH
 (setq tramp-default-method "ssh")
 
-;; Setup package archives
+;; Change default working directory
+(setq default-directory "/Users/alexandre/Documents")
+
+;; ========== Place Backup Files in Specific Directory ==========
+;; Enable backup files.
+(setq make-backup-files t)
+(setq backup-by-copying t)
+;; Enable versioning
+(setq delete-old-versions t
+  kept-new-versions 2
+  kept-old-versions 2
+  version-control t)
+;; Save all backup file in this directory.
+(setq backup-directory-alist (quote ((".*" . "~/.emacs_backups"))))
+
+;; ========== Use-Package ==========;
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")
                          ))
 
 (package-initialize)
 (unless package-archive-contents
- (package-refresh-contents))
+	(package-refresh-contents))
 
-;; ========== Use-Package ==========;
 (unless (package-installed-p 'use-package)
  (package-install 'use-package))
 
@@ -120,24 +114,41 @@
 (setq use-package-always-ensure t)
 
 ;; ========== Which-Key ==========;
-;; Install and configure which-key package
 (use-package which-key
   :ensure t)
 (which-key-mode)
 
-;; ========== Markdown mode ==========;
+;; ========== rg.el (ripgrep) ==========;
+(use-package rg
+  :ensure t)
+(rg-enable-default-bindings)
+
+;; ========== Helm ==========;
+(use-package helm
+  :ensure t)
+
+;; ========== Company ==========;
+(use-package company
+  :ensure t
+  :config)
+(add-hook 'after-init-hook 'global-company-mode) ; use company-mode in all buffers
+
+;; ========== Markdown mode ===========
 (use-package markdown-mode
   :ensure t
   :defer t
   :init
   (setq markdown-command "multimarkdown")
-  (setq markdown-hide-markup t)
   (setq markdown-header-scaling t)
-  (setq markdown-enable-wiki-links t))
+  (setq markdown-fontify-code-blocks-natively t)
+  (setq org-src-fontify-natively t)
+  (setq markdown-header-scaling t)
+  (setq markdown-enable-wiki-links t)
+)
 
 ;; ========== Org mode ==========
 (use-package org
-  :straight (:type built-in) ; tells use-package that Org mode is a built-in package and does not need to be installed separately.
+  :ensure t
   :defer t ; defers the loading of Org mode until it's actually needed.
   :config
   (setq variable-pitch-mode 1) ; changes the font to a variable-width (proportional) font,
@@ -150,18 +161,53 @@
       org-use-sub-superscripts "{}"  ; Org mode will use curly braces {} to represent subscripts and superscripts.
       org-hide-emphasis-markers t ; Org mode will hide the markers used for emphasis (bold, italic, etc.) in the buffer and only display the emphasized text
       org-hide-leading-stars t ; Org mode will hide leading stars (asterisks) that indicate heading levels. This gives a cleaner look to the buffer.
-      org-agenda-block-separator "" ; Org mode will not display separators between different agenda blocks in the agenda view.
-      org-fontify-whole-heading-line t ; Org mode will apply fontification (e.g., colors, styles) to the entire heading line
       org-fontify-done-headline t ; Org mode will fontify DONE headlines differently to visually distinguish them.
       org-fontify-quote-and-verse-blocks t ; Org mode will fontify quote and verse blocks with a different face to distinguish them from regular text.
       org-startup-with-inline-images t ; Org mode will automatically display inline images (images that are directly included in the text) when opening an Org file
-      org-image-actual-width '(600) ; sets the actual width of inline images to 600 pixels.
+      ;org-image-actual-width '(600) ; sets the actual width of inline images to 600 pixels.
       org-return-follows-link t ; pressing the RET (Enter) key on a link will follow the link instead of inserting a new line
+      org-agenda-block-separator "" ; Org mode will not display separators between different agenda blocks in the agenda view.
+      org-agenda-skip-deadline-if-done t ; skip displaying tasks in the agenda view that have deadlines and are marked as "DONE"
+      org-agenda-skip-scheduled-if-done t ; skip displaying tasks in the agenda view that are scheduled  and are marked as "DONE"
+      org-deadline-warning-days 4 ; number of days before the deadline when Org mode should issue a warning for upcoming deadlines.
+      org-agenda-todo-ignore-scheduled 'all ; ignore all scheduled tasks when generating the todo list in the agenda view
+      org-agenda-todo-ignore-with-date 'all ; ignore all tasks that have both a scheduled date and a deadline when generating the todo list in the agenda view
+      org-agenda-tags-todo-honor-ignore-options t
+      org-log-into-drawer t ; log state changes (e.g., when a task is marked as DONE) into a drawer named "LOGBOOK" in the task's properties.
+      org-log-done t ; add a timestamp and a note in the "LOGBOOK" drawer whenever a task is marked as DONE.
       )
+  (add-to-list 'org-modules 'org-habit)
+  )
 
-
-;; ========== Theme ==========;
-(use-package ef-themes
+(use-package org-superstar
   :ensure t)
-(require 'ef-themes)
-(load-theme 'ef-light :no-confirm)
+(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+
+(use-package org-modern
+  :ensure t)
+(with-eval-after-load 'org (global-org-modern-mode))
+
+;; ========== Theming ==========;
+;; Increase font size (too small for me by default)
+(set-face-attribute 'default nil :font "Inter-18")
+
+;; Customize Org Mode heading sizes
+(defun my/org-customize-heading-sizes ()
+  (dolist (face '((org-level-1 . 1.5)
+                  (org-level-2 . 1.3)
+                  (org-level-3 . 1.2)
+                  (org-level-4 . 1.1)
+                  (org-level-5 . 1.0)
+                  ;; Add more levels if needed
+                  ))
+    (set-face-attribute (car face) nil :height (cdr face))))
+(add-hook 'org-mode-hook 'my/org-customize-heading-sizes)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one-light t)
+  )
